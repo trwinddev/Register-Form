95101ca467064d591c9073cc3c758bf999246945
phongngotrong <ngotrongphong.dev@gmail.com>
2023-02-26 15:44:05 +0700

    custom input

--- src/components/form/RegisterHook.jsx
+++ src/components/form/RegisterHook.jsx
@@ -17,7 +17,7 @@ const RegisterHook = () => {
       className="max-w-[300px] mx-auto my-10"
       autoComplete="off"
     >
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-3 mb-5">
         <label htmlFor="username" className="cursor-pointer">
           Username
         </label>
@@ -29,6 +29,30 @@ const RegisterHook = () => {
           control={control}
         ></InputHook>
         <p className="text-red-500 text-sm">Please enter your name</p>
+      </div>
+      <div className="flex flex-col gap-3 mb-5">
+        <label htmlFor="email" className="cursor-pointer">
+          Email
+        </label>
+        <InputHook
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          id="email"
+          control={control}
+        ></InputHook>
+      </div>
+      <div className="flex flex-col gap-3 mb-5">
+        <label htmlFor="password" className="cursor-pointer">
+          Password
+        </label>
+        <InputHook
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          id="password"
+          control={control}
+        ></InputHook>
       </div>
       <button className="w-full bg-blue-500 rounded-lg p-5 mt-5 font-semibold text-white">
         Submit
